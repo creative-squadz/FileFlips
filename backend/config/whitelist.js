274465a5d38a54efbc081e5577ca24c1decf1ec3
creative-squadz <developer1.creativesquadz@gmail.com>
2025-12-02 17:14:22 +0530

    Update whitelist.js

--- backend/config/whitelist.js
+++ backend/config/whitelist.js
@@ -1,6 +1,7 @@
 const whitelist = [
   "http://localhost:3000",
   "https://conversionfrontend.vercel.app",
-  "https://file-flips.vercel.app"
+  "https://file-flips.vercel.app",
+  "https://www.fileflips.com"
 ];
 module.exports = whitelist;
