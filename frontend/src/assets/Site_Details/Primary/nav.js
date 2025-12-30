1c5d318c04a710641c6293071562d6ab6f2abf70
creative-squadz <developer1.creativesquadz@gmail.com>
2025-12-30 17:57:37 +0530

    Update nav.js

--- frontend/src/assets/Site_Details/Primary/nav.js
+++ frontend/src/assets/Site_Details/Primary/nav.js
@@ -1,121 +1,52 @@
 import { SiConvertio } from "react-icons/si";
-import { FaFilePdf } from "react-icons/fa";
+import { FaFilePdf, FaFileWord, FaFileExcel } from "react-icons/fa";
 import { PiArrowsMergeBold } from "react-icons/pi";
-import { FaFileWaveform } from "react-icons/fa6";
 import { MdEditDocument } from "react-icons/md";
-import { FaFileWord } from "react-icons/fa";
-import { FaFileExcel } from "react-icons/fa6";
 import { AiFillFilePpt } from "react-icons/ai";
 import { LuFiles } from "react-icons/lu";
-import { IoIosLogIn } from "react-icons/io";
+import { IoIosLogIn, IoIosLogOut } from "react-icons/io";
 import { MdAppRegistration } from "react-icons/md";
-import { IoIosLogOut } from "react-icons/io";
-//import { LuReceipt } from "react-icons/lu";
 
 export default function Nav() {
-  const list = [
+  return [
     {
       id: 1,
       name: "Converters",
-      path: "converters",
+      path: "/:email/converters",
       icon: SiConvertio,
       submenu: [
-        {
-          id: 1,
-          name: "PDF Converter",
-          path: "pdf_converter",
-          icon: FaFilePdf,
-        },
-        {
-          id: 2,
-          name: "PDF Merger",
-          path: "pdf_merger",
-          icon: PiArrowsMergeBold,
-        },
-        {
-          id: 3,
-          name: "PDF Editor",
-          path: "pdf_editor",
-          icon: MdEditDocument,
-        },
-        {
-          id: 4,
-          name: "DOCX Converter",
-          path: "docx_converter",
-          icon: FaFileWord,
-        },
-        {
-          id: 5,
-          name: "XLSX Converter",
-          path: "xlsx_converter",
-          icon: FaFileExcel,
-        },
-        {
-          id: 6,
-          name: "PPT Converter",
-          path: "ppt_converter",
-          icon: AiFillFilePpt,
-        },
+        { id: 1, name: "PDF Converter", path: "/:email/pdf_converter", icon: FaFilePdf },
+        { id: 2, name: "PDF Merger", path: "/:email/pdf_merger", icon: PiArrowsMergeBold },
+        { id: 3, name: "PDF Editor", path: "/:email/pdf_editor", icon: MdEditDocument },
+        { id: 4, name: "DOCX Converter", path: "/:email/docx_converter", icon: FaFileWord },
+        { id: 5, name: "XLSX Converter", path: "/:email/xlsx_converter", icon: FaFileExcel },
+        { id: 6, name: "PPT Converter", path: "/:email/ppt_converter", icon: AiFillFilePpt },
       ],
     },
-  
+
     {
-   // id: 2,
-    // name: "File Formats",
-    // path: "file_formats",
-    // icon: FaFileWaveform,
-      },
-    {
-    //  id: 3,
-    //  name: "My Files",
-     // path: "my_files",
-      //icon: LuFiles,
-    },
-      {
       id: 6,
       name: "Membership",
-      path: "/membership",
+      path: "/:email/membership",
       icon: LuFiles,
       submenu: [
         {
           id: 1,
           name: "My Plan",
-          path: "/home/plans",
+          path: "/:email/plans",
           icon: LuFiles,
         },
         {
           id: 2,
           name: "Invoice",
-          path: "/home/plans/templates/template1",
+          path: "/:email/invoice",
           icon: LuFiles,
         },
       ],
-      }, 
-    
-     
+    },
 
-    {
-      id: 4,
-      name: "Sign In",
-      path: "/signIn",
-      icon: IoIosLogIn,
-    },
-    {
-      id: 5,
-      name: "Sign Up",
-      path: "/signUp",
-      icon: MdAppRegistration,
-    },
-    {
-      id: 5,
-      name: "Sign Out",
-      path: "/signout",
-      icon: IoIosLogOut,
-    },
+    { id: 4, name: "Sign In", path: "/signin", icon: IoIosLogIn },
+    { id: 5, name: "Sign Up", path: "/signup", icon: MdAppRegistration },
+    { id: 7, name: "Sign Out", path: "/signout", icon: IoIosLogOut },
   ];
-   
-    
-
-
-  return list;
-} 
+}
